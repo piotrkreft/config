41d14fdecfebc8be072e44d9f9f3623959e751a8
Piotr Kreft <kreftpiotrek@gmail.com>
2024-03-01 13:16:37 +0100

    phpcs

--- tests/StorageAdapter/LocalEnvTest.php
+++ tests/StorageAdapter/LocalEnvTest.php
@@ -2,43 +2,42 @@
 
 declare(strict_types=1);
 
-namespace PK\Tests\Config\StorageAdapter
+namespace PK\Tests\Config\StorageAdapter;
+
+use PHPUnit\Framework\TestCase;
+use PK\Config\StorageAdapter\LocalEnv;
+
+class LocalEnvTest extends TestCase
 {
-    use PHPUnit\Framework\TestCase;
-    use PK\Config\StorageAdapter\LocalEnv;
+    /**
+     * @var LocalEnv
+     */
+    private $adapter;
 
-    class LocalEnvTest extends TestCase
+    protected function setUp(): void
     {
-        /**
-         * @var LocalEnv
-         */
-        private $adapter;
+        $this->adapter = new LocalEnv();
+        $_SERVER['ENV_VAR_1'] = 'env_var_value_1';
+        $_ENV['ENV_VAR_2'] = 'env_var_value_2';
+    }
 
-        protected function setUp(): void
-        {
-            $this->adapter = new LocalEnv();
-            $_SERVER['ENV_VAR_1'] = 'env_var_value_1';
-            $_ENV['ENV_VAR_2'] = 'env_var_value_2';
-        }
+    public function testShouldFetch(): void
+    {
+        // when
+        $entries = $this->adapter->fetch('some');
 
-        public function testShouldFetch(): void
-        {
-            // when
-            $entries = $this->adapter->fetch('some');
-
-            // then
-            $foundVar1 = $foundVar2 = false;
-            foreach ($entries as $entry) {
-                if ('ENV_VAR_1' === $entry->getName()) {
-                    $foundVar1 = $entry->getValue();
-                }
-                if ('ENV_VAR_2' !== $entry->getName()) {
-                    continue;
-                }
-                $foundVar2 = $entry->getValue();
+        // then
+        $foundVar1 = $foundVar2 = false;
+        foreach ($entries as $entry) {
+            if ('ENV_VAR_1' === $entry->getName()) {
+                $foundVar1 = $entry->getValue();
             }
-            $this->assertEquals('env_var_value_1', $foundVar1);
-            $this->assertEquals('env_var_value_2', $foundVar2);
+            if ('ENV_VAR_2' !== $entry->getName()) {
+                continue;
+            }
+            $foundVar2 = $entry->getValue();
         }
+        $this->assertEquals('env_var_value_1', $foundVar1);
+        $this->assertEquals('env_var_value_2', $foundVar2);
     }
 }
